841b1159e230cc83e67e097e67096b729f185517
“marrullas” <marrullas@gmail.com>
2023-11-09 11:21:03 -0500

    segundo commit curso nextjs

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+//componente de nextjs para la carga de la pagina
+
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+  return (
+    <DashboardSkeleton />
+  );
+}
